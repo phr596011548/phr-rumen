d39e22bf49bc18853698cfcfe8ee1bd5b2b59454
phr520 <596011548@qq.com>
2020-01-13 16:53:01 +0800

    克隆的项目上传第二次

--- src/main/java/cn/p/controller/Stucontroller.java
+++ src/main/java/cn/p/controller/Stucontroller.java
@@ -26,7 +26,7 @@ public class Stucontroller {
     private Stuservice stuservice;
     @Autowired
     private HttpServletRequest request;
-/*查询*/
+/*查询 哈哈哈 */
     @RequestMapping("querystu")
     @ResponseBody
     public DataTablesData querystu(Dataparams dataparams){
